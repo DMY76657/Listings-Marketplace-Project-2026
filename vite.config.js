b8239fde0d03f994e16c4b0e7c4e8f8c07aafc1b
Diyan Milev Yankov <DMY76@students.softuni.bg>
2026-03-01 14:16:59 +0100

    feat: vite.config.js multi-page build setup

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main:           resolve(__dirname, 'index.html'),
+        login:          resolve(__dirname, 'login.html'),
+        register:       resolve(__dirname, 'register.html'),
+        listingCreate:  resolve(__dirname, 'listing-create.html'),
+        listingEdit:    resolve(__dirname, 'listing-edit.html'),
+        listingDetails: resolve(__dirname, 'listing-details.html'),
+        profile:        resolve(__dirname, 'profile.html'),
+        admin:          resolve(__dirname, 'admin.html'),
+      }
+    }
+  }
+})
